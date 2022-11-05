0b5e8bd90ba8374fbadac23440d9a2e426d77b85
jamersondalvino <jamersondalvino@gmail.com>
2022-11-05 11:12:47 -0300

    exercício corrigido

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
+import React, { useState } from 'react'
+
+type InputChangeEvent = React.ChangeEvent<HTMLInputElement>
+
 const App = () => {
+  const [name, setName] = useState('')
+  const [lName, setLName] = useState('')
+  const [age, setAge] = useState('-')
+
+  const handleInputName = (event: InputChangeEvent) => {
+    setName(event.target.value)
+  }
+
+  const handleInputLName = (event: InputChangeEvent) => {
+    setLName(event.target.value)
+  }
+
+  const handleInputAge = (event: InputChangeEvent) => {
+    setAge(event.target.value)
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" onChange={handleInputName} />
       </div>
-
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" onChange={handleInputLName} />
       </div>
-
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" onChange={handleInputAge} />
       </div>
-
-      <hr/>
-
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      <hr />
+      Olá {`${name} ${lName}`}, tudo bem?
+      <br />
+      Você tem {age} anos.
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
